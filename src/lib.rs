67723715dbaac4ed30917a8a913b29a5ae348914
Charles Saracco <crsaracco@gmail.com>
2018-12-14 15:03:51 -0500

    Create a window outside an infinite loop. Window dies after drawing once.

--- src/lib.rs
+++ src/lib.rs
@@ -79,16 +79,94 @@ impl Plugin for GuiVst {
 
 
 struct GuiVstEditor {
-    window_handle: u32,
     is_open: bool,
+    polyline: Vec<xcb::Point>,
+    window_handle: u32,
+    draw_context: u32,
 }
 
 impl GuiVstEditor {
     fn new() -> Self {
         Self {
-            window_handle: 0,
             is_open: false,
+            polyline: vec![],
+            window_handle: 0,
+            draw_context: 0,
         }
+    }
+
+    fn create_window(&mut self) {
+        info!("GuiVstEditor::create_window()");
+        self.polyline.push(xcb::Point::new(50, 10 ));
+        self.polyline.push(xcb::Point::new(5, 20 ));
+        self.polyline.push(xcb::Point::new(25, -20 ));
+        self.polyline.push(xcb::Point::new(10, 10 ));
+
+        let (conn, screen_num) = xcb::Connection::connect(None).unwrap();
+        let setup = conn.get_setup();
+        let screen = setup.roots().nth(screen_num as usize).unwrap();
+
+        self.draw_context = conn.generate_id();
+
+        xcb::create_gc(&conn, self.draw_context, screen.root(), &[
+            (xcb::GC_FOREGROUND, screen.black_pixel()),
+            (xcb::GC_GRAPHICS_EXPOSURES, 0),
+        ]);
+
+        self.window_handle = conn.generate_id();
+        xcb::create_window(&conn,
+                           xcb::COPY_FROM_PARENT as u8,
+                           self.window_handle,
+                           screen.root(),
+                           0, 0,
+                           150, 150,
+                           10,
+                           xcb::WINDOW_CLASS_INPUT_OUTPUT as u16,
+                           screen.root_visual(), &[
+                (xcb::CW_BACK_PIXEL, screen.white_pixel()),
+                (xcb::CW_EVENT_MASK,
+                 xcb::EVENT_MASK_EXPOSURE | xcb::EVENT_MASK_KEY_PRESS),
+            ]
+        );
+        xcb::map_window(&conn, self.window_handle);
+        conn.flush();
+
+        let event = conn.wait_for_event();
+        match event {
+            None => { }
+            Some(event) => {
+                let r = event.response_type() & !0x80;
+                match r {
+                    xcb::EXPOSE => {
+                        self.draw_editor();
+                    },
+                    xcb::KEY_PRESS => {
+                        let key_press: &xcb::KeyPressEvent = unsafe {
+                            xcb::cast_event(&event)
+                        };
+                        info!("Key '{}' pressed", key_press.detail());
+                    },
+                    _ => {}
+                }
+            }
+        }
+    }
+
+    fn draw_editor(&mut self) {
+        info!("GuiVstEditor::draw_editor()");
+        // Draw the polyline
+        let (conn, screen_num) = xcb::Connection::connect(None).unwrap();
+        xcb::poly_line(&conn,
+                       xcb::COORD_MODE_PREVIOUS as u8,
+                       self.window_handle,
+                       self.draw_context,
+                       &self.polyline
+        );
+
+        // Flush the request
+        conn.flush();
+
+        std::thread::sleep_ms(1000);
     }
 }
 
@@ -109,51 +187,8 @@ impl Editor for GuiVstEditor {
     }
 
     fn open(&mut self, parent: *mut c_void) {
-        /*
         info!("Editor::open()");
-
-        info!("parent_handle: {}", parent as u32);
-
-        let (conn, screen_num) = xcb::Connection::connect(None).unwrap();
-
-        let setup = conn.get_setup();
-        let screen = setup.roots().nth(screen_num as usize).unwrap();
-
-        let foreground = conn.generate_id();
-        info!("Foreground id: {}", foreground);
-        xcb::create_gc(&conn, foreground, screen.root(), &[
-            (xcb::GC_FOREGROUND, screen.black_pixel()),
-            (xcb::GC_GRAPHICS_EXPOSURES, 0),
-        ]);
-
-        let win = conn.generate_id();
-        info!("Window id: {}", win);
-        xcb::create_window(
-            &conn,
-            xcb::COPY_FROM_PARENT as u8,
-            win,
-            screen.root(),
-            0, 0,
-            1000, 1000,
-            10,
-            xcb::WINDOW_CLASS_INPUT_OUTPUT as u16,
-            screen.root_visual(), &[
-                (xcb::CW_BACK_PIXEL, screen.white_pixel()),
-                (xcb::CW_EVENT_MASK,
-                 xcb::EVENT_MASK_EXPOSURE | xcb::EVENT_MASK_KEY_PRESS),
-            ]
-        );
-        xcb::map_window(&conn, win);
-        conn.flush();
-        info!("Flushed..?");
-
-        info!("looping...");
-        info!("Done looping.");
-
-        self.window_handle = win;
-        self.is_open = true;
-        */
-        CreateWindow();
+        self.create_window();
     }
 
     fn is_open(&mut self) -> bool {
@@ -162,103 +197,6 @@ impl Editor for GuiVstEditor {
     }
 }
 
-fn CreateWindow() {
-    let points: &[xcb::Point] = &[
-        xcb::Point::new(10, 10),
-        xcb::Point::new(10, 20),
-        xcb::Point::new(20, 10),
-        xcb::Point::new(20, 20),
-    ];
-    let polyline: &[xcb::Point] = &[
-        xcb::Point::new(50, 10 ),
-        xcb::Point::new( 5, 20 ),     /* rest of points are relative */
-        xcb::Point::new(25, -20),
-        xcb::Point::new(10, 10 )
-    ];
-    let segments: &[xcb::Segment] = &[
-        xcb::Segment::new(100, 10, 140, 30),
-        xcb::Segment::new(110, 25, 130, 60)
-    ];
-    let rectangles: &[xcb::Rectangle] = &[
-        xcb::Rectangle::new(10, 50, 40, 20),
-        xcb::Rectangle::new(80, 50, 10, 40)
-    ];
-    let arcs: &[xcb::Arc] = &[
-        xcb::Arc::new(10, 100, 60, 40, 0, 90 << 6),
-        xcb::Arc::new(90, 100, 55, 40, 0, 270 << 6)
-    ];
 
-
-    let (conn, screen_num) = xcb::Connection::connect(None).unwrap();
-    let setup = conn.get_setup();
-    let screen = setup.roots().nth(screen_num as usize).unwrap();
-
-    let foreground = conn.generate_id();
-
-    xcb::create_gc(&conn, foreground, screen.root(), &[
-        (xcb::GC_FOREGROUND, screen.black_pixel()),
-        (xcb::GC_GRAPHICS_EXPOSURES, 0),
-    ]);
-
-    let win = conn.generate_id();
-    xcb::create_window(&conn,
-                       xcb::COPY_FROM_PARENT as u8,
-                       win,
-                       screen.root(),
-                       0, 0,
-                       150, 150,
-                       10,
-                       xcb::WINDOW_CLASS_INPUT_OUTPUT as u16,
-                       screen.root_visual(), &[
-            (xcb::CW_BACK_PIXEL, screen.white_pixel()),
-            (xcb::CW_EVENT_MASK,
-             xcb::EVENT_MASK_EXPOSURE | xcb::EVENT_MASK_KEY_PRESS),
-        ]
-    );
-    xcb::map_window(&conn, win);
-    conn.flush();
-
-    loop {
-        let event = conn.wait_for_event();
-        match event {
-            None => { break; }
-            Some(event) => {
-                let r = event.response_type() & !0x80;
-                match r {
-                    xcb::EXPOSE => {
-                        /* We draw the points */
-                        xcb::poly_point(&conn, xcb::COORD_MODE_ORIGIN as u8, win,
-                                        foreground, &points);
-
-                        /* We draw the polygonal line */
-                        xcb::poly_line(&conn, xcb::COORD_MODE_PREVIOUS as u8, win,
-                                       foreground, &polyline);
-
-                        /* We draw the segements */
-                        xcb::poly_segment(&conn, win, foreground, &segments);
-
-                        /* We draw the rectangles */
-                        xcb::poly_rectangle(&conn, win, foreground, &rectangles);
-
-                        /* We draw the arcs */
-                        xcb::poly_arc(&conn, win, foreground, &arcs);
-
-                        /* We flush the request */
-                        conn.flush();
-
-                    },
-                    xcb::KEY_PRESS => {
-                        let key_press : &xcb::KeyPressEvent = unsafe {
-                            xcb::cast_event(&event)
-                        };
-                        println!("Key '{}' pressed", key_press.detail());
-                        break;
-                    },
-                    _ => {}
-                }
-            }
-        }
-    }
-}
 
 plugin_main!(GuiVst);
