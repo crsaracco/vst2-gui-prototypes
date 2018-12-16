4667cfaf2012c041db34357a826f0427b019e00a
Charles Saracco <crsaracco@gmail.com>
2018-12-15 22:59:32 -0500

    Break some more stuff up into smaller methods

--- src/editor.rs
+++ src/editor.rs
@@ -1,10 +1,12 @@
 use std::ffi::c_void;
+use std::cell::{RefCell, RefMut};
+use std::borrow::BorrowMut;
 use log::*;
 
 pub struct Editor {
     is_open: bool,
     polyline: Vec<xcb::Point>,
-    x_connection: Option<xcb::Connection>,
+    x_connection: Box<xcb::Connection>,
     screen_num: i32,
     window_handle: u32,
     draw_context: u32,
@@ -25,11 +27,13 @@ impl Editor {
         polyline.push(xcb::Point::new(25, -20 ));
         polyline.push(xcb::Point::new(10, 10 ));
 
+        let (conn, screen_num) = xcb::Connection::connect(None).unwrap();
+
         Self {
             is_open: false,
             polyline,
-            x_connection: None,
-            screen_num: 0,
+            x_connection: Box::new(conn),
+            screen_num,
             window_handle: 0,
             draw_context: 0,
             x: 0,
@@ -39,32 +43,34 @@ impl Editor {
         }
     }
 
-    fn create_connection(&mut self) {
-        let (conn, screen_num) = xcb::Connection::connect(None).unwrap();
-        self.x_connection = Some(conn);
-        self.screen_num = screen_num;
+    fn create_draw_context(&mut self, parent: u32) {
+        info!("GuiVstEditor::create_draw_context()");
+        let conn = self.x_connection.as_ref();
+
+        self.draw_context = conn.generate_id();
+        let draw_context = self.draw_context;
+
+        xcb::create_gc(conn, draw_context, parent, &[
+            (xcb::GC_FOREGROUND, self.get_screen().black_pixel()),
+            (xcb::GC_GRAPHICS_EXPOSURES, 0),
+        ]);
+    }
+
+    fn get_screen(&self) -> xcb::StructPtr<'_, xcb::ffi::xcb_screen_t> {
+        let conn = self.x_connection.as_ref();
+        let setup = conn.get_setup();
+        let screen = setup.roots().nth(self.screen_num as usize).unwrap();
+        screen
     }
 
     fn create_window(&mut self, parent: u32) {
         info!("GuiVstEditor::create_window()");
         info!("Parent: {}", parent);
 
-        self.create_connection();
+        self.create_draw_context(parent);
 
-        let conn = self.x_connection.as_mut().unwrap();
-
-        let setup = conn.get_setup();
-        let screen = setup.roots().nth(self.screen_num as usize).unwrap();
-
-        self.draw_context = conn.generate_id();
-
-        xcb::create_gc(&conn, self.draw_context, parent, &[
-            (xcb::GC_FOREGROUND, screen.black_pixel()),
-            (xcb::GC_GRAPHICS_EXPOSURES, 0),
-        ]);
-
-        self.window_handle = conn.generate_id();
-        xcb::create_window(&conn,
+        self.window_handle = self.x_connection.generate_id();
+        xcb::create_window(&self.x_connection,
                            xcb::COPY_FROM_PARENT as u8,
                            self.window_handle,
                            parent,
@@ -74,14 +80,14 @@ impl Editor {
                            self.height as u16,
                            0,
                            xcb::WINDOW_CLASS_INPUT_OUTPUT as u16,
-                           screen.root_visual(), &[
-                (xcb::CW_BACK_PIXEL, screen.white_pixel()),
+                           self.get_screen().root_visual(), &[
+                (xcb::CW_BACK_PIXEL, self.get_screen().white_pixel()),
                 (xcb::CW_EVENT_MASK,
                  xcb::EVENT_MASK_EXPOSURE | xcb::EVENT_MASK_KEY_PRESS),
             ]
         );
-        xcb::map_window(&conn, self.window_handle);
-        conn.flush();
+        xcb::map_window(&self.x_connection, self.window_handle);
+        self.x_connection.flush();
 
         self.draw_editor();
     }
@@ -89,20 +95,16 @@ impl Editor {
     pub fn draw_editor(&mut self) {
         info!("GuiVstEditor::draw_editor()");
 
-        // Since we draw whenever the host calls `get_editor()`, it's possible that the window
-        // wasn't created yet. Don't attempt to draw if we don't have a window yet.
-        if self.x_connection.is_some() {
-            let conn = self.x_connection.as_mut().unwrap();
-            xcb::poly_line(&conn,
-                           xcb::COORD_MODE_PREVIOUS as u8,
-                           self.window_handle,
-                           self.draw_context,
-                           &self.polyline
-            );
+        let conn = self.x_connection.as_ref();
+        xcb::poly_line(conn,
+                       xcb::COORD_MODE_PREVIOUS as u8,
+                       self.window_handle,
+                       self.draw_context,
+                       &self.polyline
+        );
 
-            // Flush the request
-            conn.flush();
-        }
+        // Flush the request
+        conn.flush();
     }
 }
 
@@ -119,7 +121,6 @@ impl vst::editor::Editor for Editor {
 
     fn close(&mut self) {
         info!("Editor::close()");
-        self.x_connection = None;
         self.is_open = false;
     }
 
