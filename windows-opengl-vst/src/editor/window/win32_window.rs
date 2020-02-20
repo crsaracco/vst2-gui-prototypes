05fc5b812aaa5f3ab363627094037d55d16e5766
Charles Saracco <crsaracco@gmail.com>
2020-02-20 18:28:36 -0500

    Update win32_window.rs

--- windows-opengl-vst/src/editor/window/win32_window.rs
+++ windows-opengl-vst/src/editor/window/win32_window.rs
@@ -21,18 +21,18 @@ impl Win32Window {
         unsafe {
             // Create a window using your window class. Store the instance in hinstance(?)
             let window_handle = user32::CreateWindowExW(
-                0,                             // TODO: what is?
+                0,                             // TODO: what is this?
                 window_class.name_win32_ptr(), // Name of the class
                 title.as_ptr(),                // Title of the window
                 dw_style,
-                winapi::CW_USEDEFAULT,    // Default X coordinate
-                winapi::CW_USEDEFAULT,    // Default Y coordinate
-                1024,                      // Default width
-                768,                      // Default height
+                winapi::CW_USEDEFAULT,         // Default X coordinate
+                winapi::CW_USEDEFAULT,         // Default Y coordinate
+                1024,                          // Default width
+                768,                           // Default height
                 parent as *mut winapi::HWND__, // Parent window
-                null_mut(),               // No menus
-                window_class.hinstance(), // The instance to the handle...?
-                null_mut(),               // TODO: what is?
+                null_mut(),                    // No menus
+                window_class.hinstance(),      // The instance to the handle...?
+                null_mut(),                    // TODO: what is this?
             );
 
             if window_handle.is_null() {
